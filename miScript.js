f0ad89031da27c57e2d088e6dfeb70c17d42d2e8
Celia Andrea Castro Díaz <80531088+CeliaAndreaCastro@users.noreply.github.com>
2021-04-20 13:56:43 -0500

    Add files via upload

---
+++ miScript.js
@@ -0,0 +1,14 @@
+var nombre='andrea';
+var apellido='castro'
+
+document.write('varible nombre: ' + nombre);
+document.write('variable apellido: ' +apellido);
+
+/* 
+
+Ete es un comentario 
+de varias lineas
+
+*/
+
+// Comentario de una sol linea
