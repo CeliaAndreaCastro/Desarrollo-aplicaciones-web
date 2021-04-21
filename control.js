ad22933fe8b24deb5a3bdef7eccb1e46102921d6
Celia Andrea Castro Díaz <80531088+CeliaAndreaCastro@users.noreply.github.com>
2021-04-20 22:42:27 -0500

    Add files via upload

---
+++ control.js
@@ -0,0 +1,9 @@
+var inputNum =Number (prompt('Ingresa un numero entero mayor de 10 y menor de 20'));
+
+if((inputNum <10) || (inputNum >20)){
+    alert('ese numero no es mayor de 10 ni menor de 20');
+}else{
+    alert('Correcto, el numero es valido');
+}
+
+document.write('<h1>Hola Mundo</h1>');
